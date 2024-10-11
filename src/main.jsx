e7cb20a93322b6adf1d12f13a6d7bd597ade339a
Juan Zarate <sebastmaster@gmail.com>
2024-10-11 11:43:55 -0300

    Primer commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { GifExpertApp } from './GifExpertApp';
+
+import './styles.css';
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <GifExpertApp />
+  </StrictMode>,
+)
